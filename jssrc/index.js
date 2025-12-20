aaf392a54a0d63557ce91ba97d088b9a6786b50a
PeekAndPoke <kontakt@karsten-gerber.de>
2025-12-20 14:33:27 +0100

    initial commit

--- jssrc/index.js
+++
@@ -1,8 +0,0 @@
-const _ = require('lodash');
-
-console.log("Hello from JS!");
-console.log("Lodash test:", _.kebabCase("Hello World From Graal"));
-
-module.exports = {
-    test: () => "JS is ready"
-};
