97f9510ec85ce88b514031f71d23b85c5f2e9748
PeekAndPoke <kontakt@karsten-gerber.de>
2026-01-08 20:28:50 +0100

    strudel ... pattern function tests

--- strudel/src/commonTest/kotlin/lang/LangAccelerateSpec.kt
+++ strudel/src/commonTest/kotlin/lang/LangAccelerateSpec.kt
@@ -2,6 +2,7 @@ package io.peekandpoke.klang.strudel.lang
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import io.peekandpoke.klang.strudel.StrudelPattern
 
 class LangAccelerateSpec : StringSpec({
 
@@ -20,5 +21,23 @@ class LangAccelerateSpec : StringSpec({
         val events = p.queryArc(0.0, 2.0)
         events.size shouldBe 4
         events.map { it.data.accelerate } shouldBe listOf(0.1, -0.2, 0.1, -0.2)
+    }
+
+    "accelerate() works within compiled code as top-level function" {
+        val p = StrudelPattern.compile("""accelerate("0 1")""")
+
+        val events = p?.queryArc(0.0, 1.0) ?: emptyList()
+
+        events.size shouldBe 2
+        events.map { it.data.accelerate } shouldBe listOf(0.0, 1.0)
+    }
+
+    "accelerate() works within compiled code as chained-level function" {
+        val p = StrudelPattern.compile("""note("a b").accelerate("0 1")""")
+
+        val events = p?.queryArc(0.0, 1.0) ?: emptyList()
+
+        events.size shouldBe 2
+        events.map { it.data.accelerate } shouldBe listOf(0.0, 1.0)
     }
 })
--- strudel/src/commonTest/kotlin/lang/LangNSpec.kt
+++ strudel/src/commonTest/kotlin/lang/LangNSpec.kt
@@ -5,5 +5,6 @@ import io.kotest.core.spec.style.StringSpec
 class LangNSpec : StringSpec({
 
     // TODO: implement tests once n() is fully implemented
+    //  @LLM do not changes this file at the moment
 
 })
