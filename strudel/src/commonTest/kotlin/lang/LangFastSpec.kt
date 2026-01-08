e8cb6272b98f595ba581b0237b740e1ac91e213f
PeekAndPoke <kontakt@karsten-gerber.de>
2026-01-08 19:59:40 +0100

    strudel ... pattern function tests

---
+++ strudel/src/commonTest/kotlin/lang/LangFastSpec.kt
@@ -0,0 +1,112 @@
+package io.peekandpoke.klang.strudel.lang
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.doubles.plusOrMinus
+import io.kotest.matchers.shouldBe
+import io.peekandpoke.klang.strudel.EPSILON
+import io.peekandpoke.klang.strudel.StrudelPattern
+
+class LangFastSpec : StringSpec({
+
+    "fast() compresses a pattern by the given factor" {
+        // Given a pattern with two sounds in one cycle, sped up by 2
+        val p = sound("bd hh").fast(2)
+
+        // When querying one cycle
+        val events = p.queryArc(0.0, 1.0).sortedBy { it.begin }
+
+        // Then the pattern plays twice (4 events total)
+        events.size shouldBe 4
+        events.map { it.data.sound } shouldBe listOf("bd", "hh", "bd", "hh")
+
+        // Each event takes 1/4 of a cycle
+        events.forEach { event ->
+            event.dur.toDouble() shouldBe (0.25 plusOrMinus EPSILON)
+        }
+    }
+
+    "fast() with factor 1 leaves pattern unchanged" {
+        // Given a pattern with factor 1
+        val p = sound("bd hh").fast(1)
+
+        // When querying one cycle
+        val events = p.queryArc(0.0, 1.0)
+
+        // Then it plays normally
+        events.size shouldBe 2
+        events.map { it.data.sound } shouldBe listOf("bd", "hh")
+        events[0].dur.toDouble() shouldBe (0.5 plusOrMinus EPSILON)
+        events[1].dur.toDouble() shouldBe (0.5 plusOrMinus EPSILON)
+    }
+
+    "fast() with large factor" {
+        // Given a pattern sped up by 4
+        val p = sound("bd").fast(4)
+
+        // When querying one cycle
+        val events = p.queryArc(0.0, 1.0)
+
+        // Then the pattern repeats 4 times
+        events.size shouldBe 4
+        events.all { it.data.sound == "bd" } shouldBe true
+        events.forEach { event ->
+            event.dur.toDouble() shouldBe (0.25 plusOrMinus EPSILON)
+        }
+    }
+
+    "fast() can be chained multiple times" {
+        // Given a pattern sped up twice
+        val p = sound("bd").fast(2).fast(2)
+
+        // When querying one cycle
+        val events = p.queryArc(0.0, 1.0)
+
+        // Then the pattern is sped up by 4 total (2 * 2)
+        events.size shouldBe 4
+        events.all { it.data.sound == "bd" } shouldBe true
+        events.forEach { event ->
+            event.dur.toDouble() shouldBe (0.25 plusOrMinus EPSILON)
+        }
+    }
+
+    "fast() with fractional factor" {
+        // Given a pattern sped up by 0.5 (which actually slows it down)
+        val p = sound("bd hh").fast(0.5)
+
+        // When querying two cycles
+        val events = p.queryArc(0.0, 2.0).sortedBy { it.begin }
+
+        // Then the pattern is stretched (plays half as fast)
+        events.size shouldBe 2
+        events.map { it.data.sound } shouldBe listOf("bd", "hh")
+        events[0].dur.toDouble() shouldBe (1.0 plusOrMinus EPSILON)
+        events[1].dur.toDouble() shouldBe (1.0 plusOrMinus EPSILON)
+    }
+
+    "fast() is inverse of slow()" {
+        // Given a pattern that is slowed then fasted by the same amount
+        val original = sound("bd hh sn")
+        val slowed = original.slow(3)
+        val fastened = slowed.fast(3)
+
+        // When querying one cycle
+        val eventsOriginal = original.queryArc(0.0, 1.0)
+        val eventsFastened = fastened.queryArc(0.0, 1.0)
+
+        // Then we get the same result
+        eventsOriginal.size shouldBe eventsFastened.size
+        eventsOriginal.map { it.data.sound } shouldBe eventsFastened.map { it.data.sound }
+    }
+
+    "fast() works within compiled code" {
+        val p = StrudelPattern.compile("""sound("bd hh").fast(2)""")
+
+        val events = p?.queryArc(0.0, 1.0) ?: emptyList()
+
+        events.size shouldBe 4
+        events.map { it.data.sound } shouldBe listOf("bd", "hh", "bd", "hh")
+        events.forEach { event ->
+            event.dur.toDouble() shouldBe (0.25 plusOrMinus EPSILON)
+        }
+    }
+})
