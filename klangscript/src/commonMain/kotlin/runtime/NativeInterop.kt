74036e70ca6601494b037d6de53e20033f62af25
codefactor-io <support@codefactor.io>
2026-02-06 12:15:20 +0000

    [CodeFactor] Apply fixes

--- klangscript/src/commonMain/kotlin/runtime/NativeInterop.kt
+++ klangscript/src/commonMain/kotlin/runtime/NativeInterop.kt
@@ -106,8 +106,7 @@ fun <T : Any> FunctionValue.convertFunctionToKotlin(): T {
     val fn = when (func.parameters.size) {
         0 -> run {
             // NOTICE: The -> is important. It defines a Function0
-            @Suppress("UNCHECKED_CAST")
-            { -> callFunction(listOf(Unit)) } as T
+            @Suppress("UNCHECKED_CAST") { -> callFunction(listOf(Unit)) } as T
         }
 
         1 -> { a1: Any? ->
