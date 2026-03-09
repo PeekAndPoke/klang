975e4457ba8b064b550f4b1a093cae07a22a613b
PeekAndPoke <kontakt@karsten-gerber.de>
2026-03-09 14:36:06 +0100

    strudel: mini notation editor tool

--- src/commonMain/kotlin/BuiltInSongs.kt
+++ src/commonMain/kotlin/BuiltInSongs.kt
@@ -152,8 +152,8 @@ stack(
         [12,19,24] [[6,13,18] [6,13,18]] [[6,13,18] [5,12,17]@2 [6,13,18]]>`).repeat(4),
     n(`<[[0,7]!4 [0,7]@4]!8 [[0,6]!4 [0,6]@4]!4 [[0,7]!4 [0,7]@4]!4 [[0,7]!4 [0,7]@4]!9 [[0,7]!4 [0,8]@4]!7>`)
   )
-  .fast(4).scale("e2:chromatic").clip(0.99).hpf(60).lpf(3000).pan(0.5).adsr("0.01:0.1:0.5:0.05")
-  .s("supersaw").unison(6).detune(0.025).gain(1.0).distort(5).postgain(0.15).warmth(0.5)
+  .fast(4).scale("e2:chromatic").clip(0.99).hpf(60).lpf(3000).pan(0.5).adsr("0.01:0.1:0.3:0.05")
+  .s("supersaw").unison(6).detune(0.025).gain(1.0).distort(5).postgain(0.175).warmth(0.5)
   .superimpose(
     x => x.bandf(120).bandq(1.5).crush(6).sound("pulse").adsr("0.01:0.05:0.25:0.0"),
     x => x.bandf(1920).bandq(1.25).crush(6).sound("pulse").adsr("0.01:0.05:0.25:0.0"),
@@ -167,15 +167,15 @@ stack(
         [32!8] [32!4 36!4] [36!8]!2 [31!8]!4 [30!8]!4 [27!8] [27!4 26!4] [26!4 25!4] [25!4 24!4]
     >`),
   )
-  .fast(4).scale("e2:chromatic").clip(0.5).hpf(60).lpf(3000).pan(0.5).adsr("0.05:0.1:0.5:0.01")
-  .s("pulse").unison(12).detune(0.01).gain(1.0).distort(5).postgain(0.175).warmth(0.5)
+  .fast(4).scale("e2:chromatic").clip(0.5).hpf(200).lpf(5000).pan(0.5).adsr("0.05:0.1:0.1:0.02")
+  .s("pulse").unison(12).detune(0.01).gain(1.0).distort(5).postgain(0.15).warmth(0.5)
   .superimpose(x => x.bandf(960).bandq(1))
    // .mute()
 
   // Drums 1 
-  , s("<hh!8!8 [oh hh!5 cr hh]!2!8>").adsr("0.00:0.1:0.5:0.1").hpf(200).postgain(1.15) // .solo()
+  , s("<hh!8!8 [oh hh!5 cr hh]!2!8>").adsr("0.00:0.1:0.5:0.1").hpf(200).postgain(1.25) // .solo()
   // Drums 2
-  , s(`<[[bd sd]!2]!4 [bd [bd,sd] bd [bd,sd]]!3 [bd bd [bd,sd] bd  bd bd [bd!2,sd] [bd!2]]!1
+  , s(`<[[bd sd]!2]!4 [bd [bd,sd] bd [bd,sd]]!3 [bd bd [bd,sd] bd  [sd!2]!4]!1
         [[bd sd]!16]!8>`).n("<7!8 0!8>").adsr("0.00:0.3:0.2:0.1").gain(0.8).hpf(30)  // . solo()
 )
   .room(0.025).rsize(5)
