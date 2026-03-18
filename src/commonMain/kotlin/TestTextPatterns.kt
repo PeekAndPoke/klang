e41ea22ef997ea2b898335e812fe23eccae15c0c
PeekAndPoke <kontakt@karsten-gerber.de>
2026-03-18 18:34:20 +0100

    base pattern class extraction

--- src/commonMain/kotlin/TestTextPatterns.kt
+++ src/commonMain/kotlin/TestTextPatterns.kt
@@ -119,11 +119,11 @@ stack(
     .filterWhen(x => x >= wait * 6 && x < (wait * 12 + keep))
   , // Lyrics -----------------------------------------------------------------------------------------------
   n("0").morse("Schön ist es auf der Welt zu sein!").orbit(0)
-    .scale("C5:major").scaleTranspose("0 -3 2 2".slow(32)).bandf(1000).bandq(2.0)
-    .sound("pulse").warmth(0.95).crush(3).gain(0.20).clip(0.5).pan(berlin.slow(2)).adsr("0.09:0.2:0.2:0.2") // .solo()
+    .scale("C5:major").scaleTranspose("0 -2 2 2".slow(32)).bandf(2000).bandq(2.0)
+    .sound("pulse").warmth(0.95).crush(3).gain(0.15).clip(0.5).pan(berlin.slow(2)).adsr("0.09:0.1:0.2:0.1") // .solo()
     .filterWhen(x => x >= wait * 8 && x < (wait * 6 + keep))
   , // Melody -----------------------------------------------------------------------------------------------
-  n("<[0 2 4 6 7 6 4 2]!14 [-1 0 2 4 6 4 2 0] [-5 -1 0 2 4 2 0 -3]>")
+  n("<[0 2 4 6 7 6 4 2]!14 [-1 0 2 4 6 4 2 0] [-3 -1 0 2 4 2 0 -3]>")
     .scale("[c3:major c3:pentatonic c3:major c3:major]/16")
     .orbit(1).s("supersaw").unison(6).detune(saw.range(0.0, 0.3).slow(16)).spread(1.0).tremolo("0.1:8").tremolodepth(saw.range(0,0.175).slow(256))
     .gain(0.3).distort(2).warmth(0.25).postgain(0.3)
