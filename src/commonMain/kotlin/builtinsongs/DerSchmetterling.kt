e77700f7a69947158f4788b3bf7ec290692f1ab9
PeekAndPoke <kontakt@karsten-gerber.de>
2026-05-17 12:04:15 +0200

    songs

--- src/commonMain/kotlin/builtinsongs/DerSchmetterling.kt
+++ src/commonMain/kotlin/builtinsongs/DerSchmetterling.kt
@@ -15,42 +15,47 @@ import * from "stdlib"                                                          
 import * from "sprudel"                                                                                                    ////
                                                                                                                           //  //
 let feel = 3.0   // 0.0 .. mechanical | 10.0 .. old vinyl                                                                //    //
-let snd  = 550   // 100 .. 2000                                                                                         //      //
+let snd  = 350 // sine.range(125, 300).slow(11).add(sine.range(125, 300).slow(13))   // 100 .. 2000                     //      //
                                                                                                                        //        //
 stack(                                                                                                    //////////////          //////////////
   // Lead                                                                                                   //                              //
   n("<[-7 0 2 4] [-7 0 4 2] [-5 -1 2 4] [-6 -1 3 1]>*2")                                                      //          DISCO!          //
-    .orbit(0).scale("e4:minor").sound("supersaw").unison(2).detune(0.05)                                        //       FOREVER!       //
-    .hpf(431).lpf(5000).warmth(0.2)                                                                               //                  //
-    .gain(0.32).distort("0.8:gentle:2").postgain(0.1) // . solo()                                                  //       //      //
-    .adsr("0.01:0.2:0.5:0.1").clip(0.8)                                                                           //     //    //    //
-    .release("<0.105!16 0.275!16 0.110!16 0.3!16 0.06!16 0.25!16 0.09!16 0.4!16 0.075!16 0.5!16>")               //   //          //  //
+    .orbit(0).scale("e4:minor").sound("supersaw").unison(8).detune(0.05)                                        //       FOREVER!       //
+    .hpf(600).lpf(1200).lpe(3.5).lpadsr("0.03:0.2:0.5:0.1").warmth(0.3)                                           //                  //
+    .gain(0.70).distort("0.1:gentle:2").postgain(0.42) // . solo()                                                 //       //      //
+    .adsr("0.03:0.15:0.7:0.1").clip(0.8)                                                                          //     //    //    //
+    .release("<0.105!16 0.4!16 0.110!16 0.325!16 0.16!16 0.35!16 0.09!16 0.4!16 0.095!16 0.5!16>")               //   //          //  //
     .superimpose(                                                                                               // //               // //
       x => x.shuffle("<1!64 0!16 1!1 4/8!14 1!33>").seed(sinOfNight.add(1).mul(24 * 60 * 15))                  //                       //
-        .superimpose(transpose(12).detune(0.07).velocity("<0!32 0.35!32>").lpf(5500).pan(0.7))
-        .superimpose(transpose(24).detune(0.10).velocity("<0!96 0.20!32>").lpf(6000).pan(0.33))
-    ).phaser(1/8).phaserdepth("<0.0!64 0.45!16 0.0!48>").phasersweep(1000).phasercenter(1500),
+        .superimpose(transpose(12).detune(0.12).velocity("<0!32 0.30!32>").lpf(1800).pan(0.3))
+        .superimpose(transpose(24).detune(0.20).velocity("<0!96 0.10!32>").lpf(2200).pan(0.7))
+    ).phaser(1/8).phaserdepth("<0.05!64 0.4!16 0.1!48>").phasersweep(1000).phasercenter(1500).mute("<0!96 1!32>"),
   // Pad
-  n("<[0 0 2 4 0 0 -2 -1]!4 [0 [2 4] 0 [2 [2 -1@3]]]!2 [0 [6 4] 0 <[2 3] [-2 -0]>] [4 [2 1] 0 [-2 <-1 -4>]]>/4")
+  n("<[0 0 2 <4 -1 4 3> 0 0 -2 -1]!4 [0 [2 4] 0 [2 [2 -1@3]]]!2 [0 [6 4] 0 <[2 3] [-2 1]>] [0 [2 1] 0 [-2 <-1 -4>]]>/4")
     .struct("<[x!16]!7 [x!24]!1 [x!16]!16>").velocity("1.02 0.95!3 0.98 0.95!3".fast(2))
-    .scale("<e2:minor!48 e3:minor!16>").sound("supersaw").unison(3).detune(0.1)
-    .notchf(snd).notchq(0.5).lpf("<1400!48 2400!16>").hpf("<140!48 240!16>").warmth(saw.range(0.3, 0.1).slow(5*32))
+    .scale("<e2:minor!48 e3:minor!16>").sound("supersaw").unison(3).detune(0.06).distort("0.1:soft:2")
+    .notchf(80).notchq(0.5).lpf("<1200!48 2000!16>").hpf("<150!48 220!16>").warmth(saw.range(0.3, 0.1).slow(5*32))
     .distort("0.3:gentle:2").distort(sine.range(0.15, 0.3).slow(10*32))
-    .adsr("0.01:0.2:0.5:0.035").clip(0.75).crush(saw.range(8, 4).slow(32))  // . solo()
-    .gain(0.35).orbit(1).pan(0.33),
+    .adsr("0.0175:0.15:0.5:0.1").clip("<0.55!48 0.7!16>").crush(saw.range(8, 4).slow(32))  // . solo()
+    .gain(0.38).orbit(1).pan(0.4)
+    .superimpose(scaleTranspose(7).gain(0.1).pan(0.2).hpf(400).lpe("1.95"), scaleTranspose(7).gain(0.1).pan(0.8).hpf(400).lpe("1.95"))
+  ,
   // Bass
   n("<0 0 2 4 0 0 -2 -1>").struct("<[x!8]!14 [x!12]!2 [x!8]!32>").fast(2).velocity("1.02 0.95!3 0.98 0.95!3".fast(2))
-    .scale("e2:minor").sound("saw").warmth(saw.range(0.3, 0.1).slow(5*32))
-    .notchf(snd).notchq(0.5).lpf("1000").hpf(90).distort("0.2.75:gentle:2")
-    .adsr("0.01:0.2:0.5:0.045").clip(0.75)  // . solo()
-    .gain(0.42).orbit(2).pan(0.66),
+    .scale("<e2:minor!28 e3:minor!4>")
+    .sound("saw").warmth(saw.range(0.3, 0.1).slow(5*32))
+    .notchf(snd).notchq(0.5).lpf(saw.range(800, 1000).slow(4)).hpf(90).distort("0.1:soft:2")
+    .adsr("0.015:0.15:0.5:0.1").clip("<0.55!48 0.7!16>") //  . solo()
+    .gain(0.4).orbit(2).pan(0.5)
+    .superimpose(scaleTranspose(4).gain(0.25).pan(0.1).hpf(160).lpe(1.5), scaleTranspose(4).gain(0.25).pan(0.9).hpf(160).lpe(1.5))
+  ,
   // Drums
   sound("<[bd!2]!2 [bd!4]!2 [bd!8]!2 [bd!16] [bd!24] [bd sd bd sd]!24 [bd [bd,sd] bd [bd,sd]]!8>") // . solo()
-    .orbit(3).gain(0.8).crush(10).crushos(2).hpf(60).lpf("2400:1").lpe("<2!128 2.3!128 2.7!128>").adsr("0.01:0.2:0.5:0.1"),
-  sound("<[hh hh oh hh]!24 [cr hh cr hh]!16>") //. solo()
-    .orbit(4).gain(0.4).fast(2).crush(8).crushos(2).hpf(400).lpf("3500:1:2").adsr("0.01:0:1.0:0.1")
+    .early(0.002).orbit(3).gain(0.95).crush(10).crushos(2).hpf(80).lpf("2000:1:1.5").lpe("<2!128 2.3!128 2.7!128>").adsr("0.01:0.2:0.5:0.1"),
+  sound("<[hh hh oh hh]!48 [cr hh cr hh]!16 [0 hh 0 hh]!32>") //. solo()
+    .early(-0.002).orbit(4).gain(0.8).fast(2).crush(10).crushos(2).hpf(800).lpf("3700:1:1.5").adsr("0.01:0.2:0.5:0.1")
   // Master
-).compressor("-10:2:10:0.02:0.25").analog(feel).engine("pedal").room("0.01:5")
+).compressor("-6:2:10:0.02:0.25").analog(feel).engine("pedal").room("0.01:5")
 
 
 // Inspired by: Editors - Papillon
@@ -61,7 +66,8 @@ stack(                                                                          
 
 // Written by: peekandpoke
 
-// Epilspsy Warning: Do not click the oscilloscope!
+// Epilepsy Warning: Do not click the oscilloscope!
+
 
 
 
