d04241d8d1cc5ed3d7c4479d06dc6c4c16a54680
PeekAndPoke <kontakt@karsten-gerber.de>
2026-06-06 18:54:18 +0200

    sprudel query performance optimization: introduced mutable SprudelVoiceData to avoid copy overhead

--- src/commonMain/kotlin/builtinsongs/DerSchmetterling.kt
+++ src/commonMain/kotlin/builtinsongs/DerSchmetterling.kt
@@ -22,7 +22,7 @@ stack(                                                                          
   n(`<[-7 0 2 4] [-7 0 4 [2 6]|[4 2]|2|2|2|2] [-5 -1 2 4] [-6 -1 [4 3]|5|3|3|3 [1 -1]|1|1|1|1]>*2`)                //          DISCO!          //
     .orbit(1).scale("<e4:minor!48 e5:minor!16>").sound("superramp").unison(11).detune(0.03).analog(feel)             //       FOREVER!       //
     .hpf(1350).lpf(1600).lpe(berlin.range(2.0, 2.0)).lpq(1.2).lpadsr("0.005:3.0:0.5:0.05")                             //                  //
-    .gain(1.00).distort("0.300:tube:4").postgain("<0.800!48 0.325!16>") // . solo()                                     //       //      //
+    .gain(1.00).distort("0.300:tube:4").postgain("<0.850!48 0.325!16>") // . solo()                                     //       //      //
     .adsr("0.01:3.0:0.0:0.05").clip(0.8)  // . mute()                                                                  //     //    //    //
     .release("<0.10!16 0.25!16>")                                                                                     //   //          //  //
     .phaser(1/8).phaserdepth(0.1).phasersweep(1000).phasercenter(1800)                                               // //               // //
@@ -47,7 +47,7 @@ stack(                                                                          
   n("<0 0 2 4 0 0 -2 -1>") //  . solo()
     .scale("<e2:minor>").struct("<[x!8]!14 [x!12]!2 [x!8]!32>").fast(2)
     .analog(feel).sound("supersaw").unison(15).detune(0.08)
-    .hpf(180).hpq(1.0).lpf(1600).lpe(1.0).lpq(2.50)
+    .hpf(180).hpq(1.0).lpf(1600).lpe(1.0).lpq(2.00)
     .adsr("0.009:3.0:0.0:0.005").adsrCurves("square:exp:cube").lpadsr("0.007:1.0:0.0:0.005").velocity("1.00 0.95!3 0.98 0.95!3".fast(2))
     .clip("<0.96!31 0.9 0.96!31 0.825>".fast(2)).gain(0.8).distort("1:tube:4").distort(1.0)
     .coarse(2).coarseos(2)  
