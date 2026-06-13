4ffeae4d8f76cdb0dd53063607d96da4bd96e2db
PeekAndPoke <kontakt@karsten-gerber.de>
2026-06-13 02:22:55 +0200

    - engine dsl - songs

--- src/commonMain/kotlin/builtinsongs/StrangerThings.kt
+++ src/commonMain/kotlin/builtinsongs/StrangerThings.kt
@@ -34,21 +34,26 @@ stack(
     .orbit(1).s("supersaw").unison(3).detune(saw.range(0.0, 0.35).slow(16)).spread(1.0 ).tremolo("0.1:8").tremolodepth(saw.range(0,0.1).slow(256))
     .gain(0.8).distort(0.25).warmth(0.7).postgain(0.25).adsr("0.01:0.2:0.8:0.155")
     .pan(sine.range(0.3, 0.7).slow(16)) // . solo()
-    .superimpose(bandf(notch).bandq(1.0).gain(0.125).transpose(12))
-    .hpf(350).lpf(800).lpenv(perlin.range(3.0, 4.5).slow(8)).analog(3)
+    .superimpose(bandf(notch).bandq(1.0).gain(0.125).transpose(12)).body("glass")
+    .hpf(350).lpf(800).lpenv(perlin.range(3.0, 4.5).lpq(1.5).slow(8)).analog(3)
     .filterWhen(x => x >= wait * 4 && x < (wait * 4 + keep)) // . solo()
   , // Bass -----------------------------------------------------------------------------------------------------------------------------
-  note("<a1 [f1 c2 e1 [f2 c2]] [a1 [c2 f1] a1 [f1@3 e1]] [a1@2 c2@3 d2 [c2,c3] [d1,d1,d2]]>/8").clip(0.8).struct("x!8")
-    .orbit(2).s("supersaw").unison(4).detune(saw.range(0.05, 0.45).slow(64)).warmth(0.3)
-    .gain(1.0).adsr("0.01:0.6:0.5:0.4").postgain(0.4).pan(saw.range(0.5, 0.1).slow(keep * 2))
-    .superimpose(x => x.orbit(3).scaleTranspose("<[12 12 7 12 12 [12 12] 0 -12] [12 12 0 12 12 [0 12] 0 -12]>/16").pan(saw.range(0.5, 0.9).slow(keep * 2)).legato(1.05))
-    .lpf(4 * 440).hpf(90).notchf(notch).notchq(0.75).analog(2)
-    .superimpose(x => x.gain(saw.slow(64).pow(1.5).mul(2.0)).crush("1.5".add(berlin2.mul(0.25).slow(4))).lpf(4.5 * 440).hpf(300).postgain(0.4).late(0.002))
-    .velocity(cat(saw.pow(2).slow(32), pure(1).slow(256)).mul("1 0.95 0.975 0.95".fast(2))).analog(1.5)  // . solo()
+  note("<a1 [f1 c2 e1 [f2 c2]] [a1 [c2 f1] a1 [f1@3 e1]] [a1@2 c2@3 d2 [c2,c3] [d1,d1,d2]]>/4").clip(0.8).struct("x!4").slow(16)
+    .orbit(2).s("supersaw").unison(4).detune(saw.range(0.05, 0.45).slow(64)).warmth(0.1)
+    .gain(1.0).adsr("0.01:0.6:0.8:1.80").postgain(0.4)
+    .superimpose(
+      x => x.orbit(3).scaleTranspose("<[12 12 7 12 12 [12 12] 0 -12] [12 12 0 12 12 [0 12] 0 -12]>/32")
+        .pan(saw.range(0.5, 0.9).slow(keep * 2)).clip(0.9)
+    ).lpf(4 * 440).lpq(2.5).hpf(90).notchf(notch).notchq(0.75).analog(2).body("glass")
+    .superimpose(
+      x => x.gain(saw.range(0.2, 1.0).slow(64).pow(1.5).mul(2.5)).vibrato(16).vibmod(0.2)
+        .crush("1.85".add(berlin2.mul(0.5).slow(4))).crushos(2).lpf(4.5 * 440).hpf(300).postgain(0.4)
+        .pan(0.2).superimpose(pan(0.8))                 
+    ).velocity(cat(saw.pow(2).slow(32), pure(1).slow(256)).mul("1 0.95 0.975 0.95".fast(2))).analog(1.5)  // . solo()
     .filterWhen(x => x < (wait * 4 + keep)) // . mute()
   , // Perc 2 ------------------------------------------------------------------------------------------------------------------
   sound("<[hh hh oh hh] [hh hh ~ hh] [hh hh oh hh] [hh hh ~ <cr!7 rd>]>")
-    .orbit(4).gain(0.6).pan(0.4).adsr("0.04:0.2:0.8:2.0").fast(2).degrade(0.1).lpf(4800).late(0.002)
+    .orbit(4).gain(0.6).pan(0.4).adsr("0.04:0.2:0.8:2.0").fast(2).degrade(0.1).lpf(4800).late(0.0005)
     .filterWhen(x => x >= wait * 1 && x < (wait * 2 + keep))
   , // Perc 1 -----------------------------------------------------------------------------------------------------------------------
   sound("[bd bd bd ~  bd ~ bd ~] [bd bd sd:5 ~  bd ~ bd|sd:5 ~]").slow("[8 8 8 8 8 8 4 [2 4]]/32").fast(2)
@@ -59,7 +64,7 @@ stack(
     .orbit(0).gain(0.08).pan(perlin.early(1.7).range(0.3, 0.7).slow(21)).adsr("0.2:1.0:1.0:2.5")
     .bandf(perlin.range(440, 440 * 4).segment(16).slow(48)).bandq(sine.range(0.05, 5.0).slow(32).early(16))
   ,
-).delay("0.1::0.5").delaytime(pure(1/8).div(cps)).room("0.05:10.0").compressor("-15:2:6:0.01:0.2")
+).delay("0.2::0.5").delaytime(pure(1/8).div(cps)).room("0.05:10.0").compressor("-15:2:6:0.01:0.2")
 
 
 
