778a5737d3d560b45f5a2d3868ebf77ee9e2dab5
PeekAndPoke <kontakt@karsten-gerber.de>
2026-08-08 03:48:02 +0200

    songs

--- src/commonMain/kotlin/builtinsongs/DerSchmetterling.kt
+++ src/commonMain/kotlin/builtinsongs/DerSchmetterling.kt
@@ -25,14 +25,14 @@ stack(                                                                          
   // Lead                                                                                                                     //////////////          //////////////.
   n(`<[-7 0 2 4] [-7 0 4 [2 6]|[4 2]|2|2|2] [-5 -1 2 4] [-6 -1 [4 3]|4|3|3|3 [1 -1]|1|1|1|1]>*2`)                               //                              //.
     .orbit(0).scale("<e4:minor!48 e5:minor!16 e4:minor!48 e3:minor!16>").sound("superramp").unison(15).spread(0.08)               //          DISCO!          //.
-    .hpf(1500).lpf(1850).lpe(berlin.range(1.7, 1.9).fast(4)).lpq(2.3).lpadsr("0.010:0.2:0.8:0.03")                                 //       FOREVER!       //.
-    .gain(0.60).distort("0.570:tube:4").postgain("<0.200!48 0.095!16 0.200!48 0.300!16>") // . solo()                                 //                  //.
+    .hpf(1400).lpf(1850).lpe(berlin.range(1.7, 1.9).fast(4)).lpq(2.3).lpadsr("0.010:0.2:0.8:0.03")                                  //       FOREVER!       //.
+    .gain(0.60).distort("0.530:tube:4").postgain("<0.200!48 0.100!16 0.200!48 0.300!16>") // . solo()                                 //                  //.
     .adsr("0.010:4.0:0.2:0.03").clip(0.90).release("<0.08!16 0.15!16>").vibrato(8).vibmod(0.01)  // . mute()                           //       //      //.
     .shuffle("<1!64 0!16 1!1 4/8!14 1!33>")                                                                                           //     //.   //    //.
     .superimpose(x => x.transpose(12).spread(0.12).mute("<1!16 0!16>").velocity(0.25).pan(0.35).superimpose(pan(0.65)))              //   //.         //  //.
     .mute("<1!32 0!192>").pipeline("pedal").room("0.3:5:0.1")                                                                       // //.              // //.
   , // Guitar 1                                                                                                                    //.                      //.
-  n(`<[0 [0@6 3 6] [4 [0 2]] [3 1] [-3 -1 2 4] [7 -3] -2 <[-1 0@3] [4 6@3] [[3 4] 3@3] [[4 6] 7@3]>]!4                                
+  n(`<[0 [0@6 3 6] [4 [3 2]] [3 1] [-3 -1 2 4] [7 -3] -2 <[-1 0@3] [4 7@3] [[3 4] 3@3] [[4 6] 7@3]>]!4                                
       [[4@2 [2 0] 0] [-1 -4] [-3 1 -3 1 -3!10 1 -3] [2 [2 6@3]]]!2
       [[-3,-7] [[-4,-5] [-1,-3]] [0,-3] <[[4 6],[0 -1]] [0,-1]>] [<[7,4] [[7 4 6 2  7 4 6 2]!2]> [-5 -6] [-7,-14] [-5 <-1 -4 -4 1>]]>/4`)
     .orbit(1).scale("<e3:minor!48 e4:minor!16 e3:minor!48 e4:minor!16>").struct("<[x!16]!7 [x!24]!1 [x!16]!16>") //  .mute()
@@ -56,7 +56,7 @@ stack(                                                                          
     ).mute("<0!128 1!16 0!16>").pipeline("pedal").body("spruce").bodyMix(0.3)
   , // Bass
   n("<0 0 2 4 0 0 -2 -1>").struct("<[x!1]!16 [x@3 x]!48 [x!4]!80>").fast(2).velocity("0.98 0.94 0.96 0.94".fast(2))  // . mute()
-    .orbit(3).scale("e1:minor").sound("sine").gain(1.0).distort("0.1:soft:4").postgain(0.100).clip(0.66)
+    .orbit(3).scale("e1:minor").sound("sine").gain(1.0).distort("0.05:soft:4").postgain(0.100).clip(0.66)
     .adsr("0.010:4.0:0.0:0.010").lpadsr("0.005:0.06:0.0:0.01").hpf(45).lpf(120).lpe(30).lpq(0.7)  //  .solo()
     .pan(0.35).superimpose(pan(0.65)).mute("<0!128 1!32>") // .pipeline("pedal")
   , // Drums
@@ -67,7 +67,7 @@ stack(                                                                          
     .superimpose(x => x.bandf("205".add(berlin.mul(10).fast(4))).bandq(4).vel(0.60).hpf(190).lpf(350)),
   sound("<[hh hh hh hh]!16 [hh hh oh hh]!24 [cr hh cr hh]!24 [~ rd ~ rd]!32>").fast(2).mute("<0!128 1!32>") // . solo()
     .pan(0.525).late(0.0030).orbit(5).gain(0.26).hpf(800).lpf("10500".add(perlin.mul(300).fast(4))).adsr("0.005:0.15:0.8:0.2"), // . mute()
-  sound("<~!79 [~ ~ ~ cp  cp ~ cp ~] ~!47 [~ ~ ~ cp  cp ~ cp ~]>").orbit(6).gain(0.13).mute("<0!128 1!32>"),
+  sound("<~!79 [~ ~ ~ cp  cp ~ cp ~] ~!47 [~ ~ ~ cp  cp ~ cp ~]>").orbit(6).gain(0.10).mute("<0!128 1!32>"),
   sound("pink!8").orbit(7).gain(0.04).hpf(8000).lpf(13500).lpq(0.5)
     .pan(sine.range(0.4, 0.6).slow(11)).adsr("0.005:0.15:0.0:0.05")  // .solo()
   // Master
