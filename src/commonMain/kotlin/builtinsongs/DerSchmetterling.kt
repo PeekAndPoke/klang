62728880ab00c2c7afe729c02f1235230d3e4159
PeekAndPoke <kontakt@karsten-gerber.de>
2026-08-11 00:56:55 +0200

    songs

--- src/commonMain/kotlin/builtinsongs/DerSchmetterling.kt
+++ src/commonMain/kotlin/builtinsongs/DerSchmetterling.kt
@@ -18,7 +18,7 @@ internal val derSchmetterlingSong = Song(
     code = """import * from "stdlib"
 import * from "sprudel"
 
-let feel = 18.0    // 0.0 .. guitar | 100.0 .. rave
+let feel = 12.0    // 0.0 .. guitar | 100.0 .. rave
 
 let supersawHp = (() => {
 
@@ -34,11 +34,11 @@ let supersawHp = (() => {
   let signal = Osc.supersaw(freq = Osc.freq(), voices = pVoices, spread = pSpread)
     // character knobs first — plain scalars, SuperSaw-typed, must precede the filter
     .analog(pAnalog).spreadPower(2.0).sideAtten(0.6).gainJitter(0.85).centerJitter(0.05)
-    .add(Osc.supersaw(freq = Osc.freq().mul(2), voices = pVoices, spread = pSpread).mul(0.25))
+    .add(Osc.supersaw(freq = Osc.freq().mul(2), voices = pVoices, spread = pSpread).mul(0.2))
     .highpass(Osc.freq().mul(pHpTrack), pHpQ, pAnalog)
    
   return signal.add(signal.bandpass(1500, 0.500).mul(1.5))
-    .lowpass(5500)
+    .lowpass(5600)
 
 })()
                                                                                                                        
@@ -47,7 +47,7 @@ stack(
   n(`<[-7 0 2 4] [-7 0 4 [2 6]|[4 2]|2|2|2] [-5 -1 2 4] [-6 -1 [4 3]|[5 3]|3|3|3 [1 -1]|1|1|1|1]>*2`)                  
     .orbit(0).scale("<e4:minor!48 e5:minor!16 e4:minor!48 e3:minor!16>").sound(supersawHp).unison(15).spread(0.08)    
     .hpf(800).lpf(1800).lpe(perlin.range(1.9, 2.1).fast(2)).lpq(1.5).lpadsr("0.010:0.5:0.5:0.03")                      
-    .gain(0.50).distort("0.440:tube:4").postgain("<0.200!48 0.110!16 0.200!48 0.280!16>") // . solo()                  
+    .gain(0.50).distort("0.460:tube:4").postgain("<0.200!48 0.110!16 0.200!48 0.280!16>")  // . solo()                  
     .adsr("0.010:4.0:0.5:0.03").clip(0.95).release("<0.08!16 0.15!16>").vibrato(8).vibmod(0.01)  // . mute()            
     .shuffle("<1!64 0!16 1!1 4/8!14 1!33>")                                                                            
     .superimpose(x => x.transpose(12).spread(0.12).mute("<1!16 0!16>").velocity(0.25).pan(0.35).superimpose(pan(0.65)))
@@ -60,7 +60,7 @@ stack(
     .velocity("0.98 0.95!7 0.97 0.95!7".fast(2)) //  . solo()
     .sound(supersawHp).unison(11).spread(0.05).gain(0.5).postgain(0.14).distort("1:tube:4").distort(0.80)    
     .clip("<0.93!31 0.85 0.93!31 0.83 0.93!30 0.85 0.80>".fast(2)).adsr("0.004:3.0:0.0:0.035").lpadsr("0.004:1.0:0.0:0.025")    
-    .hpf("<600>").lpf("3000".add(saw.range(1, 0).pow(4.0).mul(200)).slow(8)).lpe(1.2).lpq(0.7)
+    .hpf("<600>").lpf("3000".add(saw.range(1, 0).pow(4.0).mul(200)).slow(8)).lpe(1.2).lpq(0.8)
     .coarse(2).coarseos(4).pan(0.15).superimpose(pan(0.80)).body("spruce").bodyMix(0.4)
   , // Guitar 2
   n("<0 0 2 4 0 0 -2 -1>") //   . solo()
@@ -68,9 +68,9 @@ stack(
     .velocity("0.98 0.95!7 0.97 0.95!7".fast(2))
     .sound(supersawHp).unison(13).spread(0.08).gain(0.5).postgain(0.12).distort("1:tube:4").distort(0.80)
     .clip("<0.93!31 0.85 0.93!31 0.83 0.93!30 0.85 0.80>".fast(2)).adsr("0.009:3.0:0.0:0.035").lpadsr("0.009:1.0:0.0:0.025")    
-    .hpf(80).lpf(2600).lpe(1.0).lpq(0.7)
+    .hpf(80).lpf(2600).lpe(1.0).lpq(0.8)
     .coarse(3).coarseos(4).pan(0.5).superimpose(
-      x => x.pan(0.65), x => x.pan(0.35), 
+      x => x.pan(0.65), x => x.pan(0.35),
       x => x.postgain(0.11).hpf(300).lpf(2600).transpose(12).scaleTranspose("<4!7 [2 [3 4@3]]!1 4!7 [0 -3] -3!7 [-5 [-4 -3@3]]!1 -3!7 [-3 [2 4@3]]>")
            .pan(0.2).superimpose(pan(0.8))
     ).mute("<0!128 1!16 0!16>").body("mahogany").bodyMix(0.4)
@@ -87,7 +87,7 @@ stack(
     .superimpose(x => x.bandf("205".add(berlin.mul(10).fast(4))).bandq(4).vel(0.80).hpf(180).lpf(450)),
   sound("<[hh hh hh hh]!16 [hh hh oh hh]!24 [cr hh cr hh]!24 [~ rd ~ rd]!32>").fast(2).mute("<0!128 1!32>") // . solo()
     .pan(0.525).late(0.0030).orbit(5).gain(0.21).hpf(1000).lpf("11500".add(perlin.mul(300).fast(4))).adsr("0.005:0.05:0.9:0.2"), // . mute()
-  sound("<~!79 [~ ~ cp cp  cp ~ cp ~] ~!47 [~ ~ cp cp  cp ~ cp ~]>").orbit(6).gain(0.10).mute("<0!128 1!32>"),
+  sound("<~!79 [~ ~ ~ cp  cp ~ cp ~] ~!47 [~ ~ ~ cp  cp ~ cp ~]>").orbit(6).gain(0.10).mute("<0!128 1!32>"),
   sound("pink!8").orbit(7).gain(0.03).hpf(8000).lpf(15000).lpq(0.5)
     .pan(sine.range(0.4, 0.6).slow(11)).adsr("0.005:0.15:0.0:0.05"),  // .solo()
   sound("white!3").orbit(7).gain(0.001).hpf(8000).lpf(15000).lpq(0.5)
@@ -106,7 +106,6 @@ stack(
 // Written by: peekandpoke
 
 // Epilepsy Warning: Do not click the oscilloscope!
-
 
 
 
